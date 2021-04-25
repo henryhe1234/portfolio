52ad23a4daeb448f53824127608b4b1852a67f77
henryhe1234 <henryhe1123@gmail.com>
2021-04-25 14:03:17 -0600

    small fix2

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -38,24 +38,7 @@ export default function SinglePost() {
     <main className="bg-gray-200 min-h-screen p-12">
       <article className="container shadow-lg mx-auto bg-green-100 rounded-lg">
         <header classNmae="relative">
-          {/* <div className="absolute h-full w-full flex items-center justify-center p-8">
-            <div className="bg-white bg-opacity-75 rounded p-12">
-              <h1 className="cursive text-3xl lg:text-6xl mb-4">
-                {singlePost.title}
-              </h1>
-              <div className="flex justify-center text-gray-800">
-                <img
-                  src={urlFor(singlePost.authorImage).url()}
-                  alt={singlePost.name}
-                  className="w-10 h-10 rounded-full"
-                />
-
-                <p className="cursive flex items-center pl-2 text-2xl">
-                  {singlePost.name}
-                </p>
-              </div>
-            </div>
-          </div> */}
+          
           <img
             src={singlePost.mainImage.asset.url}
             alt={singlePost.title}
